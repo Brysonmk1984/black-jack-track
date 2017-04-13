06e6abff21c695ea6d70aa5d68542d1d85e3a647
Bryson Kruk <brysonmk1984@gmail.com>
2017-04-13 10:15:28 -0600

    added app.js

--- scraper.js
+++ scraper.js
@@ -1,15 +1,36 @@
 const https = require('https');
+const http = require('http');
 const odds = "https://wizardofvegas.com/guides/blackjack-survey/";
 const test = "http://brysonkruk.com";
 
-https.get(odds, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
+module.export = {
+	scrapeOdds : function(){
+		https.get(odds, (res) => {
+		  console.log('statusCode:', res.statusCode);
+		  console.log('headers:', res.headers);
 
-}).on('error', (e) => {
-  console.error(e);
-});
+		  res.on('data', (d) => {
+		    process.stdout.write(d);
+		  });
+
+		}).on('error', (e) => {
+		  console.error(e);
+		});
+	},
+	scrapeTest : function(){
+		http.get(test, (res) => {
+		  console.log('statusCode:', res.statusCode);
+		  console.log('headers:', res.headers);
+		  alert(11);
+		  res.on('data', (d) => {
+		    process.stdout.write(d);
+		  });
+
+		}).on('error', (e) => {
+		  console.error(e);
+		});
+	}
+};
+
+
